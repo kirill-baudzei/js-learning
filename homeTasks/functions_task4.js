d6ab01f11249e980cf11740b73e5780308df94c2
Kirill <kirillbaudzei@gmail.com>
2024-09-08 21:32:26 +0200

    new code

--- homeTasks/functions_task4.js
+++ homeTasks/functions_task4.js
@@ -70,4 +70,29 @@ function findMaxNumber(a, b, c, d) {
 // fib(0) => ''"
 
 // решение
-function fib() {}
+function fib(num) {
+  if (typeof num !== "number" || num <= 0 || !Number.isInteger(num)) {
+    return "";
+  }
+
+  let result = "";
+  let first = 0;
+  let second = 1;
+
+  for (let i = 0; i < num; i++) {
+    if (i + 1 === num) {
+      result += `${first}`;
+      // Без пробела в конце
+    } else {
+      result += `${first} `;
+    }
+
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return result;
+}
+
+fib(5);
