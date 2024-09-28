6a3d7f964267b631da4d11b6192aeb9f6658f292
Kirill Mac <kirillbaudzei@gmail.com>
2024-09-28 14:57:41 +0200

    new code

--- fileHomeTask/js/script.js
+++ fileHomeTask/js/script.js
@@ -24,3 +24,38 @@ const movieDB = {
     ]
 };
 
+// 1  1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const adv = document.querySelectorAll(".promo__adv img"),
+      genre = document.querySelector(".promo__genre"),
+      poster = document.querySelector('.promo__bg'),
+      movieList = document.querySelector('.promo__interactive-list');
+console.log(adv);
+adv.forEach( item => {
+    item.remove();
+});
+
+// другой вид функции (не стрелочная)
+// adv.forEach( function (item) {
+//     item.remove();
+// });
+
+// 2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+console.log(genre);
+genre.textContent = "ДРАМА";
+
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+// Реализовать только при помощи JS
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+//4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту 
+movieList.innerHTML = "";
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
