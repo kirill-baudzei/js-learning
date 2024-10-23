6092d9d4a9ac8324e1b1bacde48bc1b76f63ba26
Kirill Mac <kirillbaudzei@gmail.com>
2024-10-23 15:57:47 +0200

    new file

---
+++ theory/this.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// 1 и 2
+// function showThis() {
+//     console.log(this);
+// }
+// showThis(); // undefined из-за strict . window - если use strcit нет. (глобальный объект)
+// 3
+// function User(name, id,) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}! You are ${this.id}!!!`) // this везде ссылается на объект 
+//     };
+// }
+
+// const kirill = new User('Kirill', 22);
+
+// 4
+function sayName(surname) {
+    console.log(this); // выводит кон
+    console.log(this.name + surname);
+}
+
+const user = {
+    name: 'John'
+};
+
+sayName.call(user, 'Smith');
+sayName.apply(user, 'Smith');
+
+function count(num) {
+    return this*num;
+}
+
+const double = count.bind(2); // определяет что будет передаваться в this - определяем контекст
+
+// 1)  Обычная функция - this = window, но если use strict - undefined
+// 2) Контекст у объектов метода - всегда сам объект
+// 3) This в конструкторах и классах - это новый экземпляр объекта 
+// 4) 
