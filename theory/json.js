a6edeb92ed6ca330ee76137166a17078eed94689
Kirill Mac <kirillbaudzei@gmail.com>
2024-10-30 19:40:23 +0100

    new code

---
+++ theory/json.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const persone = {
+    name: 'alex',
+    tel: '+134112312',
+    parents: {
+        mom: 'mom',
+        dad: 'dad'
+    }
+};
+
+console.log(JSON.stringify(persone)); // {"name":"alex","tel":"+134112312"} - получаем json
+
+// если получаем json
+const weReceiveJson = JSON.stringify(persone);
+console.log(JSON.parse(weReceiveJson)); // снова получаем объект { name: 'alex', tel: '+134112312' }
+
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'ANN';
+console.log(persone);
+console.log(clone);
---
+++ theory/symbols.js
@@ -0,0 +1,11 @@
+// тип данных символ
+'use strict'
+
+const obj = {
+    'name': "test"
+}
+
+let id = Symbol("id");
+obj[id] = 1;
+
+console.log(obj);
