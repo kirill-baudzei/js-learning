2ce96d4bbabacd1ab7db114466352a7719a95932
Kirill Mac <kirillbaudzei@gmail.com>
2024-11-23 17:16:00 +0100

    new code

--- js_landingPractise/js/script.js
+++ js_landingPractise/js/script.js
@@ -182,12 +182,21 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    //         });
+    //     });
+    
+    // запрос с библиотекой axios
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({ img, altimg, title, descr, price }) => {
                 new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
             });
         });
+
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => createCard(data));
