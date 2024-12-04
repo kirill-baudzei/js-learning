3e86a86405774ce088c5570e0e9689deadacb8c4
Kirill Mac <kirillbaudzei@gmail.com>
2024-12-04 20:17:50 +0100

    new code

---
+++ theory/getterAndSetter.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const persone = {
+    name: 'Alex',
+    age: 25,
+
+    get userAge() {
+        return this.age
+    },
+
+    set userAge(number) {
+        this.age = number;
+    }
+};
+
+console.log(persone.userAge = 30); 
+
+console.log(persone.userAge);
